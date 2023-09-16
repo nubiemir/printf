1ed89541d7f723f0fd165ab47d771bf44e03d86d
feraqary <135108973+feraqary@users.noreply.github.com>
2023-09-16 06:33:21 +0000

    added the evaulation format

--- ft_printf.c
+++ ft_printf.c
@@ -1,17 +1,10 @@
 #include <stdarg.h>
 #include "printf.h"
 
-int flag_checker(char c)
-{
-    if (c == '#' || c == ' ' || c == '+' || c == '0' || c == '.' || c == '-')
-        return (1);
-    return (0);
-}
-
 int format_checker(char c)
 {
     if (c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X' || c == 'c' || c == 's' || c == 'p' || c == '%')
-        return (1);
+        return (1); 
     return (0);
 }
 
@@ -20,31 +13,38 @@ char *handle_width()
     return ("hello");
 }
 
-void handle_flag(char c, char *str)
+int handle_format(Format *format, char *str, int counter)
 {
+    while (str[counter] != 'u' && str[counter] != 'i' && str[counter] != 'd' && str[counter] != 'x' && str[counter] != 'X' && str[counter] != 'c' && str[counter] != 's' && str[counter] != 'p' && str[counter] != '%')
+    {
+        if (str[counter] == '.' || str[counter] == ' ' || str[counter] == '0' || str[counter] == '-' || str[counter] = '#' || str[counter] == '+')
+        {
+            format->flag = str[counter];
+        }
+        counter++;
+    }
+    format->type = str[counter]
+    return (counter);
 }
 
 int ft_printf(const char *ptr, ...)
 {
     va_list args;
-    va_start(args, ptr);
     unsigned int counter;
+    Format *format;
     int res;
 
+    va_start(args, ptr);
     counter = 0;
     res = 0;
+    if(!(format = (Format *)malloc(sizeof(Format))))
+        return (0);
     while (ptr[counter])
     {
         if (ptr[counter] != '%')
             res += write(1, &ptr[counter], 1);
         else
-        {
-            write(1, "\n", 1);
-            if (flag_checker(ptr[++counter]))
-                handle_flag(ptr[counter], &ptr[counter]);
-            else if (format_checker(ptr[counter]))
-                printf("format found %c\n", ptr[counter]);
-        }
+            i = handle_format(format, ptr, i+1)
         counter++;
     }
     return (res);
--- printf.h
+++ printf.h
@@ -1,15 +1,16 @@
 #ifndef PRINTF_H
-#define PRINTF_H
+# define PRINTF_H
 
 #include <stdio.h>
 #include <unistd.h>
 
 typedef struct FormatConversion
 {
-    char flag;
-    char *width;
-    char  type;
+    char    flag;
+    char    *width;
+    char    type;
 } Format;
 
 int ft_printf(const char *ptr, ...);
+
 #endif
